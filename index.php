51ae7c63b2e9ab943886121e7dfd4ae3b138e77e
LucaCaligola <luca.caligola@gmail.com>
2024-01-16 16:38:00 +0100

    i create all the infrastructure to call the API, now i'm going to put the data inside it

--- index.php
+++ index.php
@@ -7,39 +7,62 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
 </head>
 <body>
 
-    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 
 
-        <div id="app">
+    <div id="app">
+    <div class="container">
 
-
-
-            Spotify
-
-
-
-
-
-
+            <div class="row">
+                <div class="col-12">
+                    <ul class="list-group">
+                        <li class="list-group-item" v-for="(element, index) in dischi">
+                            {{ element }}
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
+
+
+
+    </div>
         
-    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
     
+   
 </body>
 </html>
 
 
 <script>
-    const { createApp } = Vue
-  
-    createApp({
-      data() {
-        return {
-          
-        }
+  const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+
+        apiUrl : './server.php',
+        dischi : [],
       }
-    }).mount('#app')
-  </script>
+    },
+
+    methods: {
+        getDischiList(){
+            axios.get(this.apiUrl).then((response) =>{ 
+                this.dischi = (response.data)
+
+                console.log(response.data)
+            })
+        }
+    },
+
+    created (){
+        this.getDischiList();
+    }
+  }).mount('#app')
+</script>
